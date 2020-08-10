4c9270be4180ea4a30228ce3e9c603be71ba3507
1003082940@qq.com <Dzyu@123!>
2020-08-11 01:05:17 +0800

    commit message:jms中的activemq中的broker测试 committer:NirvanaYDZ branch:master

---
+++ src/main/java/com/study/activemq/improve/broker/EmbedBroker.java
@@ -0,0 +1,12 @@
+package com.study.activemq.improve.broker;
+
+import org.apache.activemq.broker.BrokerService;
+
+public class EmbedBroker {
+    public static void main(String[] args) throws Exception {
+        BrokerService brokerService = new BrokerService();
+        brokerService.setUseJmx(true);
+        brokerService.addConnector("tcp://localhost:61616");
+        brokerService.start();
+    }
+}
