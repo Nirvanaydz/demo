8028227fe08f6d580d76c9fe59647316210ba7fa
1003082940@qq.com <Dzyu@123!>
2020-08-12 01:15:20 +0800

    commit message:demo中的代码测试类 committer:NirvanaYDZ branch:master

---
+++ src/main/java/test/MainTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.time.ZonedDateTime;
+
+public class MainTest {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        Class clazz = MainTest.class;
+        System.out.println(zonedDateTime);
+    }
+}
